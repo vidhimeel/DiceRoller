680a6d199cf26dcd3d4412be77299476d84eb1ec
vidhimeel <vidhimeel@gmail.com>
2024-03-25 21:52:40 +0530

    Image & Spacer Composable added.

--- app/src/main/java/com/vidhi/diceroller/MainActivity.kt
+++ app/src/main/java/com/vidhi/diceroller/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vidhi.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +59,8 @@ fun DiceWithButtonAndImage(modifier: Modifier) {
             painter = painterResource(R.drawable.dice_1),
             contentDescription = "1"
         )
-        Button(onClick = { /*TODO*/ }) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {  }) {
             Text(stringResource(R.string.roll))
         }
     }
